4b58ce5c6c52c135de689096d0b16134c891d4e5
sowogy <90501828+sowogy@users.noreply.github.com>
2026-03-23 13:18:21 +0900

    Update SecurityConfig.java
    BCrypt 강도 8로 변경

--- src/main/java/com/example/UrbanismWebSite/config/SecurityConfig.java
+++ src/main/java/com/example/UrbanismWebSite/config/SecurityConfig.java
@@ -90,6 +90,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(10);
+        return new BCryptPasswordEncoder(8);
     }
 }
